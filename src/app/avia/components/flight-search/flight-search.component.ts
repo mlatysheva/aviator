b7f457eb238763fb81ca8f5ef4471349c409d17b
anna <ani.kolesnikova@gmail.com>
2023-04-21 14:23:38 +0200

    feat: add search form, avia service init

--- src/app/avia/components/flight-search/flight-search.component.ts
+++ src/app/avia/components/flight-search/flight-search.component.ts
@@ -1,26 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
-
-const today = new Date();
-const month = today.getMonth();
-const year = today.getFullYear();
+import { AviaService } from '../../services/avia.service';
 
 @Component({
   selector: 'app-flight-search',
   templateUrl: './flight-search.component.html',
   styleUrls: ['./flight-search.component.scss'],
 })
-export class FlightSearchComponent {
-  public campaignOne = new FormGroup({
-    start: new FormControl(new Date(year, month, 13)),
-    end: new FormControl(new Date(year, month, 16)),
-  });
+export class FlightSearchComponent implements OnInit {
+  public roundTrip = false;
+  public oneWay = true;
 
-  public campaignTwo = new FormGroup({
-    start: new FormControl(new Date(year, month, 15)),
-    end: new FormControl(new Date(year, month, 19)),
-  });
+  public searchForm: FormGroup;
 
-  toppings = new FormControl('');
-  toppingList: string[] = ['Adult', 'Child', 'Infant'];
+  passengers = new FormControl('');
+  passengersList: string[] = ['Adult', 'Child', 'Infant'];
+
+  constructor(private aviaService: AviaService) {}
+
+  ngOnInit() {
+    this.searchForm = new FormGroup({
+      tripType: new FormControl('round-trip'),
+      departure: new FormControl(''),
+      destination: new FormControl(''),
+      dates: new FormControl(''),
+      passengers: new FormControl(''),
+    });
+  }
+
+  public changeValues(): void {
+    const departure = this.searchForm.get('departure')?.value;
+    const destination = this.searchForm.get('destination')?.value;
+    this.searchForm.controls['departure'].setValue(destination);
+    this.searchForm.controls['destination'].setValue(departure);
+  }
+
+  public onSearch() {
+    this.aviaService.search();
+  }
 }
