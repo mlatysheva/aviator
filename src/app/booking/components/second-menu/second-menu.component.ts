df6f9c31747b5e14fbad0e17062ecaf5bb2c998c
Olena Honcharuk <gasmin07@gmail.com>
2023-04-26 09:29:50 -0700

    feat: implement routing to booking page

--- src/app/booking/components/second-menu/second-menu.component.ts
+++ src/app/booking/components/second-menu/second-menu.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-second-menu',
   templateUrl: './second-menu.component.html',
   styleUrls: ['./second-menu.component.scss']
 })
-export class SecondMenuComponent {
+export class SecondMenuComponent implements OnInit {
+  from: string;
+  to: string;
+  date: string;
+  people: number;
+
+  onClick() {
+    this.from === 'From' ? this.from = 'To' : this.from = 'From';
+    this.to === 'To' ? this.to = 'From' : this.to = 'To';
+  }
+  ngOnInit() {
+    this.from = 'From';
+    this.to = 'To';
+    this.date = 'Date';
+    this.people = 1;
+  }
+
 
 }
