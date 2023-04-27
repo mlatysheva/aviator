917b33744e0a64b31f0be4a9cdb7b5e672e4f72d
mlatysheva <mlatysheva@gmail.com>
2023-04-27 23:42:38 +0200

    feat: add menu icon change to logout on successful login

--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -4,7 +4,9 @@ import { HttpClient } from '@angular/common/http';
 import { IUser } from '../../models';
 import { baseUrl } from '../../constants/apiUrls';
 import { Router } from '@angular/router';
-import { USER_ID } from '../../constants/localStorage';
+import { USER_EMAIL, USER_ID, USER_NAME } from '../../constants/localStorage';
+import { Store } from '@ngrx/store';
+import { clearUserState } from '../../store/actions/user.actions';
 
 
 @Injectable({
@@ -18,21 +20,37 @@ export class AuthService {
     !!localStorage.getItem(USER_ID),
   );
 
-  userId$ = localStorage.getItem(USER_ID) || '';
+  userName$ = new BehaviorSubject<string>(
+    localStorage.getItem(USER_NAME) || localStorage.getItem(USER_EMAIL) || '',
+  );
 
   constructor(
     private http: HttpClient,
+    private store: Store,
   ) {}
 
   onLogin(login: string, password: string) {
     const response = this.http.post(`${baseUrl}/login`, { login, password });
     response.subscribe((userData: IUser) => {
       if (userData.id) {
-        this.userId$ = userData.id;
-        localStorage.setItem(USER_ID, this.userId$);
-        this.isAuth$.next(true);
-        this.isVisible$.next(false);
+        localStorage.setItem(USER_ID, userData.id);
       }
+      if (userData.firstName) {
+        localStorage.setItem(USER_NAME, userData.firstName);
+      } 
+      if (userData.email) {
+        localStorage.setItem(USER_EMAIL, userData.email);
+      }
+      this.isAuth$.next(true);
+      this.isVisible$.next(false);
     });
+  }
+
+  onLogout() {
+    localStorage.removeItem(USER_ID);
+    localStorage.removeItem(USER_NAME);
+    localStorage.removeItem(USER_EMAIL);
+    this.store.dispatch(clearUserState());
+    this.isAuth$.next(false);
   }
 }
--- src/app/constants/localStorage.ts
+++ src/app/constants/localStorage.ts
@@ -1,6 +1,11 @@
 export const CURRENCY = 'aviator_currency';
+
 export const DATE_FORMAT = 'aviator_date_format';
 
 export const TRIP_TYPE = 'aviator_trip_type';
 
 export const USER_ID = 'aviator_user_id';
+
+export const USER_EMAIL = 'aviator_user_email';
+
+export const USER_NAME = 'aviator_user_name';
