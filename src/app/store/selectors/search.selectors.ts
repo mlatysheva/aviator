dfcd92241410fa4c29475618e4583b39853c2b0f
anna <ani.kolesnikova@gmail.com>
2023-05-04 16:03:44 +0200

    feat: add whole search form selector

--- src/app/store/selectors/search.selectors.ts
+++ src/app/store/selectors/search.selectors.ts
@@ -2,14 +2,6 @@ import { createSelector } from '@ngrx/store';
 import { IAgeTypeQuantity } from 'src/app/avia/models/agetype-quantity.model';
 import { AppState } from '../state.models';
 
-// export const selectSearchFormValue = (state: AppState) => {
-//   state.search.tripType;
-//   state.search.departure;
-//   state.search.destination;
-//   state.search.startDate;
-//   state.search.endDate;
-//   state.search.passengers;
-// };
 export const selectTripType = (state: AppState) => state.search.tripType;
 export const selectDeparture = (state: AppState) => state.search.departure;
 export const selectDestination = (state: AppState) => state.search.destination;
@@ -37,4 +29,21 @@ export const selectDates = createSelector(
 export const selectPersons = createSelector(
   selectPassengers,
   (passengers: IAgeTypeQuantity[]) => passengers
+);
+
+export const selectAllSearchDetails = createSelector(
+  selectTripType,
+  selectDeparture,
+  selectDestination,
+  selectStartDate,
+  selectEndDate,
+  selectPassengers,
+  (
+    tripType: string,
+    departure: string,
+    destination: string,
+    startDate: string,
+    endDate: string,
+    passengers: IAgeTypeQuantity[]
+  ) => [tripType, departure, destination, startDate, endDate, passengers]
 );
