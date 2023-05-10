b07a959b740c819172c6f65af516e474cbcce407
mlatysheva <mlatysheva@gmail.com>
2023-05-10 10:29:52 +0200

    feat: add edit/delete btns

--- src/app/cart/pages/cart-page/cart-page.component.ts
+++ src/app/cart/pages/cart-page/cart-page.component.ts
@@ -1,16 +1,16 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { Store } from '@ngrx/store';
-import { CellClickedEvent, ColDef, GridReadyEvent, ValueGetterParams } from 'ag-grid-community';
+import { CellClickedEvent, ColDef, GridReadyEvent, ICellRendererParams, SelectCellEditor, ValueGetterParams } from 'ag-grid-community';
 import { AppState } from '../../../store/state.models';
 import { HttpClient } from '@angular/common/http';
 import { CartApiService } from '../../services/cart-api.service';
-import { USER_ID } from '../../../constants/localStorage';
-import { ICart } from '../../../models/cart';
 import { ITrip } from '../../../models';
-import { Observable, Subscription } from 'rxjs';
+import { Observable, Subscription, map } from 'rxjs';
 import { AgGridAngular } from 'ag-grid-angular';
-import { NewLineKind } from 'typescript';
 import { Router } from '@angular/router';
+import { selectUserCurrency } from '../../../store/selectors/user.selectors';
+import { state } from '@angular/animations';
+
 
 @Component({
   selector: 'app-cart-page',
@@ -21,93 +21,178 @@ import { Router } from '@angular/router';
 export class CartPageComponent implements OnInit {
   cartId = '00e0d78e-b1e7-4099-a1c7-7b73cd92d12f';
   cart$: Subscription;
-  // cart$ = this.cartApiService.getCart(this.cartId);
-  trips: ITrip[] = [];
   trips$: Observable<ITrip[]>;
+  totalPrice$: Observable<number>;
+
+  currency = this.store.select(selectUserCurrency);
 
   @ViewChild(AgGridAngular) agGrid!: AgGridAngular;
-
-  public colDefs: ColDef[] = [
-    {
-      headerName: 'No.',
-      headerCheckboxSelection: true,
-      checkboxSelection: true,
-      width: 120,
-      showDisabledCheckboxes: true,
-      cellStyle: { color: '#0090BD', 'fontWeight': '700'},
-      valueGetter: flightNosGetter,
-    },
-    { 
-      headerName: 'Flight',
-      filter: true,
-      floatingFilter: false,
-      cellRenderer: flightGetter,
-    },
-    {
-      headerName: 'Trip Type',
-      width: 140,
-      valueGetter: tripTypeGetter,
-      sortable: true,
-      unSortIcon: true
-    },
-    {
-      headerName: 'Date & Time',
-      minWidth: 230,
-      cellRenderer: dateTimeGetter,
-      sortable: true,
-      unSortIcon: true,
-    },
-    {
-      headerName: 'Passengers',
-      width: 140,
-      cellRenderer: passengersGetter,
-    },
-    { 
-      headerName: 'Price',
-      width: 120,
-      field: 'totalAmount',
-      cellStyle: { color: "#7F8906" },
-      sortable: true,
-      unSortIcon: true
-    },
-    {
-      width: 70,
-      cellRenderer: moreActionsRenderer,
-      cellStyle: { cursor: 'pointer' },
-      onCellClicked: this.showActionsMenu.bind(this),
-    }
-  ];
-
-  defaultColDef: ColDef = {
-    resizable: true,
-    autoHeight: true,
-    wrapText: true, 
-    wrapHeaderText: true,
-    autoHeaderHeight: true,    
-    cellStyle: { 
-      padding: '1rem', 
-      lineHeight: '1.5rem', 
-      height: '100%',
-      display: 'flex',
-      justifyContent: 'center',
-      alignItems: 'center',
-      fontWeight: '500',
-    },
-  };
+  colDefs: ColDef[];
+  defaultColDef: ColDef;
 
   constructor(
     private router: Router,
+    private store: Store<AppState>,
     private http: HttpClient,
     private cartApiService: CartApiService,
-  ) {}
+  ) {
+    this.colDefs = [
+      {
+        headerName: 'No.',
+        headerCheckboxSelection: true,
+        checkboxSelection: true,
+        width: 120,
+        showDisabledCheckboxes: true,
+        cellStyle: { color: '#0090BD', 'fontWeight': '700'},
+        valueGetter: flightNosGetter,
+      },
+      { 
+        headerName: 'Flight',
+        filter: true,
+        floatingFilter: false,
+        cellRenderer: flightGetter,
+      },
+      {
+        headerName: 'Trip Type',
+        width: 140,
+        valueGetter: tripTypeGetter,
+        sortable: true,
+        unSortIcon: true
+      },
+      {
+        headerName: 'Date & Time',
+        minWidth: 230,
+        cellRenderer: dateTimeGetter,
+        sortable: true,
+        unSortIcon: true,
+      },
+      {
+        headerName: 'Passengers',
+        width: 140,
+        cellRenderer: passengersGetter,
+      },
+      { 
+        headerName: 'Price',
+        width: 120,
+        field: 'totalAmount',
+        cellStyle: { color: "#7F8906" },
+        sortable: true,
+        unSortIcon: true
+      },
+      {
+        headerName: 'More Actions',
+        width: 70,
+        editable: false,
+        cellEditorPopup: true,
+        colId: 'moreActions',
+        // cellRenderer: this.renderEditButton,
+        cellRenderer: actionCellRenderer,
+        // cellRenderer: this.getSelectedValue.bind(this),
+        cellStyle: { cursor: 'pointer' },
+        onCellClicked: this.showActionsMenu.bind(this),
+        cellEditor: 'agSelectCellEditor',
+        cellEditorParams: {
+            values: ['Delete', 'Edit'],
+            handleGridCellEvent: this.onDataChangeHandler.bind(this),
+        }
+      },
+    ];
+  
+    this.defaultColDef = {
+      resizable: true,
+      autoHeight: true,
+      wrapText: true, 
+      wrapHeaderText: true,
+      autoHeaderHeight: true,    
+      cellStyle: { 
+        padding: '1rem', 
+        lineHeight: '1.5rem', 
+        height: '100%',
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontWeight: '500',
+      },
+    };
+  }
+
+
+  private onDataChangeHandler() {
+    console.log('onDataChangeHandler');
+  }
 
   ngOnInit(): void {
-    this.cart$ = this.cartApiService.getCart(this.cartId);
-    this.trips$ = this.cartApiService.getTripsByCartId(this.cartId);
+    console.log('we are in ngOnInit');
   }
 
   onGridReady(params: GridReadyEvent) {
-    // this.trips$ = this.cartApiService.getTripsByCartId(this.cartId);
+    console.log('we are in onGridReady');
+    this.cart$ = this.cartApiService.getCart(this.cartId);
+    this.trips$ = this.cartApiService.getTripsByCartId(this.cartId);
+    this.currency = this.store.select(state => state.user.currency);
+    console.log(this.currency);
+
+
+    this.totalPrice$ = this.trips$.pipe(
+      map((trips) => {
+        console.log(trips);
+        const price =  trips.reduce((acc, trip) => acc + trip.totalAmount, 0);
+        console.log(price);
+        return price;
+      }
+    ));
+  }
+
+  onRowEditingStarted(params: any) {
+    params.api.refreshCells({
+      columns: ["action"],
+      rowNodes: [params.node],
+      force: true
+    });
+  }
+
+  onRowEditingStopped(params: any) {
+    params.api.refreshCells({
+      columns: ["action"],
+      rowNodes: [params.node],
+      force: true
+    });
+  }
+  
+  onCellClicked(params: any) {
+    console.log(params.column.colId);
+    console.log(params.event.target.dataset);
+    if (params.column.colId === "moreActions" && params.event.target.dataset.action) {
+      const action = params.event.target.dataset.action;
+      console.log('we are in moreActions');
+
+      if (action === "edit") {
+        params.api.startEditingCell({
+          rowIndex: params.node.rowIndex,
+          // gets the first columnKey
+          colKey: params.columnApi.getDisplayedCenterColumns()[0].colId
+        });
+      }
+
+      if (action === "delete") {
+        params.api.applyTransaction({
+          remove: [params.node.data]
+        });
+      }
+
+      if (action === "update") {
+        params.api.stopEditing(false);
+      }
+
+      if (action === "cancel") {
+        params.api.stopEditing(true);
+      }
+    }
+  }
+
+  renderEditButton() {
+    const eIconGui = document.createElement('span');         
+    return  eIconGui.innerHTML = '<em class="material-icons">more_vert</em>';          
   }
 
   showActionsMenu(event: CellClickedEvent) {
@@ -157,26 +242,26 @@ function passengersGetter (params: ValueGetterParams) {
 
 function moreActionsRenderer() {
   return '<i class="material-icons">more_vert</i>'
-  // const eIconGui = document.createElement('div');         
-  // return eIconGui.innerHTML = `
-  //   <button mat-icon-button [matMenuTriggerFor]="menu" aria-label="Example icon-button with a menu">
-  //     <mat-icon>more_vert</mat-icon>
-  //   </button>
-  //   <mat-menu #menu="matMenu">
-  //     <button mat-menu-item>
-  //       <mat-icon>dialpad</mat-icon>
-  //       <span>Redial</span>
-  //     </button>
-  //     <button mat-menu-item disabled>
-  //       <mat-icon>voicemail</mat-icon>
-  //       <span>Check voice mail</span>
-  //     </button>
-  //     <button mat-menu-item>
-  //       <mat-icon>notifications_off</mat-icon>
-  //       <span>Disable alerts</span>
-  //     </button>
-  //   </mat-menu>
-  // `
+  const eIconGui = document.createElement('div');         
+  return eIconGui.innerHTML = `
+    <button mat-icon-button [matMenuTriggerFor]="menu" aria-label="Example icon-button with a menu">
+      <mat-icon>more_vert</mat-icon>
+    </button>
+    <mat-menu #menu="matMenu">
+      <button mat-menu-item>
+        <mat-icon>dialpad</mat-icon>
+        <span>Redial</span>
+      </button>
+      <button mat-menu-item disabled>
+        <mat-icon>voicemail</mat-icon>
+        <span>Check voice mail</span>
+      </button>
+      <button mat-menu-item>
+        <mat-icon>notifications_off</mat-icon>
+        <span>Disable alerts</span>
+      </button>
+    </mat-menu>
+  `
 } 
 
 // <mat-icon>more_vert</mat-icon>
@@ -205,3 +290,4 @@ function actionCellRenderer(params: any) {
 
   return eGui;
 }
+
